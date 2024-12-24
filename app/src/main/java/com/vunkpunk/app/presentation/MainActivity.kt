3dc7311b9cef1d16f94f795f9077bd6e2839c3f8
wrdx <wrdxwrdxwrdx@gmail.com>
2024-12-25 00:54:23 +0300

    fix: remove o in MainActivity

--- app/src/main/java/com/vunkpunk/app/presentation/MainActivity.kt
+++ app/src/main/java/com/vunkpunk/app/presentation/MainActivity.kt
@@ -40,7 +40,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         var start = Screen.SearchScreen.route + "/{$PARAM_SEARCH}"
-        if (TOKEN.value == "o"){
+        if (TOKEN.value == ""){
             start = Screen.WelcomeScreen.route
         }
         super.onCreate(savedInstanceState)
